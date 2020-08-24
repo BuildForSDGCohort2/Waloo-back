02cb5320863b2261258630f01cabe64c6bb92266
sopegue <yayasopeguesoro@gmail.com>
2020-08-24 02:15:15 +0100

    updated .env

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://stark-garden-63780.herokuapp.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
